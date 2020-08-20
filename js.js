6b74ce45956cfbb7994905a4bfb476e64c574c40
cordelia013 <cordeliaguims@yahoo.com>
2020-08-20 12:03:29 +0200

    mise a jour du tableau apres correction du brief et cerification norme W3C

---
+++ js.js
@@ -0,0 +1,17 @@
+// je recupère l'élément
+
+
+function toggleColor(){
+    var test = document.getElementById("trs");
+  
+    console.log(test.style.backgroundColor);
+    /*if( getComputedStyle(test).backgroundColor == '#a1d9e6'){
+        getComputedStyle(test).backgroundColor ='#FF2FDA';
+    }
+   else{  getComputedStyle(test).backgroundColor  = '#53BDFF';} */
+}
+console.log("réussi");
+toggleColor();
+
+
+
